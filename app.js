6bd6698de1194341ad0d36e9726dee09fc1ddcde
Ayoub-Haj-Husein <ayoubabdulrahmanhusein@gmail.com>
2023-10-08 00:44:52 +0300

    edit on problem solving[7]

--- app.js
+++ app.js
@@ -65,14 +65,94 @@ Use for and forEach and then map to
 solve this question to see the difference between the three ways 
 */
 
-// firs way
-let number = [1, 2, 3];
-function power(arr) {
-  let newArr = [];
-  for (let i = 0; i < arr.length; i++) {
-    newArr.push(2 ** arr[i]);
-  }
-  return newArr;
-}
+// firs way (for)
 
-console.log(power(number));
+// let number = [1, 2, 3];
+
+// function power(arr) {
+//   let newArr = [];
+//   for (let i = 0; i < arr.length; i++) {
+//     newArr.push(2 ** arr[i]);
+//   }
+//   return newArr;
+// }
+// console.log(power(number));
+
+// second way (forEach)
+
+// let number = [1, 2, 3];
+
+// function power(arr) {
+//   let newArr = [];
+//   arr.forEach((el) => {
+//     newArr.push(2 ** el);
+//   });
+//   return newArr;
+// }
+// console.log(power(number));
+
+// therd way (map)
+
+// let number = [1, 2, 3];
+
+// function power(arr) {
+//   let result = arr.map((el) => {
+//     return 2 ** el;
+//   });
+//   return result;
+// }
+// console.log(power(number));
+
+/*
+6- Write a function that, takes an array of numbers as input, 
+uses map to return a new array where each element is either 
+the string "even" or the string "odd", based on each value.
+If any element in the array is not a number, the resulting 
+array should have the string "N/A" in its place.
+For example:([1,2,3,"Rawan"]) returns ['odd','even','odd','N/A'].
+*/
+
+// let elements = [1, 2, 3, "Rawan"];
+// function oddEven(arr) {
+//   let result = arr.map((el) => {
+//     if (typeof el == "string") {
+//       return "N/A";
+//     } else if (el % 2 !== 0) {
+//       return "odd";
+//     } else if (el % 2 === 0) {
+//       return "Even";
+//     }
+//   });
+//   return result;
+// }
+// console.log(oddEven(elements));
+
+/*
+7- Write a function named fizzbuzz that takes in an array of numbers.
+Iterate over the array using forEach or map to determine the output 
+based on several rules:
+  - If a number is divisible by 3, add the word "Fizz" to the output array.
+  - If the number is divisible by 5, add the word "Buzz" to the output array.
+  - If the number is divisible by both 3 and 5, add the phrase 
+    "Fizz Buzz" to the output array.
+  - Otherwise, add the number to the output array.
+Return the resulting output array.
+*/
+
+const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 30];
+
+function fizzbuzz(arr) {
+  let result = arr.map((el) => {
+    if (el % 3 === 0 && el % 5 !== 0) {
+      return "Fizz";
+    } else if (el % 5 === 0 && el % 3 !== 0) {
+      return "Buzz";
+    } else if (el % 3 === 0 && el % 5 === 0) {
+      return "Fizz Buzz";
+    } else {
+      return el;
+    }
+  });
+  return result;
+}
+console.log(fizzbuzz(numbers));
